0bc9bd9d9513c17b4154b3190e6e9c266eb3f570
ThirdTitan <jackmcleanwilson@gmail.com>
2025-07-22 12:15:43 +0100

    Fixed dynamic list

--- Templates/Nimbus-Filmgrain/js/list.js
+++ Templates/Nimbus-Filmgrain/js/list.js
@@ -8,7 +8,17 @@ const list = document.getElementById("projectlist")
 for (const name of projectNames) {
     console.log(name);
 
-    const entry = document.createElement("p");
+    const entry = document.createElement("a");
+    entry.setAttribute("href", "#");
+    entry.setAttribute("class", "hover-underline-animation");
+    entry.setAttribute("id", "projectlink");
+    entry.setAttribute("onclick", "") //Put method here
+    
     entry.textContent = name;
-    list.appendChild(p);
+    list.appendChild(entry);
+}
+
+for(var i = 0; i < 2; i++){
+    const pagebreak = document.createElement("BR"); 
+    document.getElementById("projectlink").insertBefore(pagebreak, null); 
 }
